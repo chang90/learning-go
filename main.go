bbc76f5fcc837903c4c84b215bc2494b2d6f0ffd
hiby90hou <chang.liu.programmer@gmail.com>
2021-04-19 22:34:33 +1000

    add variable

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import "log"
 
 func main() {
-	fmt.Println("Hello world!")
+	var whatToSay string
+
+	// _ means the second param, but we do not need it
+	whatToSay, _ = saySomething("Hi")
+
+	log.Println(whatToSay)
+}
+
+func saySomething(say string) (string, string) {
+	return say, "world"
 }
